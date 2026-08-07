1f13b988e7f0ed436ae5a7d938127ad521066fcd
Grace Luvanda <Grcluvanda@gmail.com>
2026-08-07 07:54:17 -0400

    Switch to HashRouter for GitHub Pages deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Explore from "./pages/Explore";
 import Author from "./pages/Author";
